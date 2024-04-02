cc64343c5c145a4ef313eb313971bfbfcef9bf53
Calvin Studebaker <calstudebaker@gmail.com>
2024-04-01 23:28:46 -0700

    add 2024 films

---
+++ docs/js/dropdown.js
@@ -0,0 +1,16 @@
+function openDropdown() {
+    document.getElementById("film-dropdown").classList.toggle("dropdown-show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-btn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('dropdown-show')) {
+                openDropdown.classList.remove('dropdown-show');
+            }
+        }
+    }
+}
